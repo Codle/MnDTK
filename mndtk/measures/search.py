354814828c5b0f66968bfe6cf6967ec137be8216
Codle <codle@outlook.com>
2020-07-02 22:09:48 +0800

    init

--- mndtk/measures/search.py
+++ mndtk/measures/search.py
@@ -8,10 +8,19 @@ def mrr(ranks):
     return np.mean(1.0 / ranks)
 
 
+def cg(rels: np.array) -> np.float:
+    return sum(rels)
+
+
+def dcg(rels: np.array, ranks: np.array) -> np.float:
+    return np.sum(rels / np.log2(ranks+1))
+
+
 def pairwise_vec_mrr(src_vec: np.ndarray,
                      tgt_vec: np.ndarray,
                      distance_metric: str) -> Tuple[np.array, np.array]:
-
+    """ 两个向量矩阵首先计算距离，根据距离排序并计算MRR得分，常用于语义匹配模型中
+    """
     distances = cdist(src_vec, tgt_vec, metric=distance_metric)
     # By construction the diagonal contains the correct elements
     correct_elements = np.expand_dims(np.diag(distances), axis=-1)
---
+++ mndtk/test/test_search.py
@@ -0,0 +1,13 @@
+from mndtk.measures import search as search_utils
+import numpy as np
+
+rels = np.array([3, 2, 3, 0, 1, 2])
+ranks = np.array([1, 2, 3, 4, 5, 6])
+
+
+def test_cg():
+    assert search_utils.cg(rels) == 11
+
+
+def test_dcg():
+    assert f"{search_utils.dcg(rels, ranks):.3f}" == "6.861"
